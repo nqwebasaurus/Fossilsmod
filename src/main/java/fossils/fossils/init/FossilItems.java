d39108819260038832c234e870b2fa896406baf7
superlord9362 <33046398+superlord9362@users.noreply.github.com>
2025-03-25 08:33:43 -0700

    Changed Item Rarity
    Added custom item rarities

--- src/main/java/fossils/fossils/init/FossilItems.java
+++ src/main/java/fossils/fossils/init/FossilItems.java
@@ -3,7 +3,6 @@ package fossils.fossils.init;
 import fossils.fossils.FossilMod;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Rarity;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -15,17 +14,17 @@ public class FossilItems {
 	public static final RegistryObject<BlockItem> TROPAEUM = REGISTER.register("tropaeum_fossil", () -> new BlockItem(FossilBlocks.TROPAEUM.get(), new Item.Properties()));
 	
 	//Five
-	public static final RegistryObject<BlockItem> EUNOTOSAURUS = REGISTER.register("eunotosaurus_fossil", () -> new BlockItem(FossilBlocks.EUNOTOSAURUS.get(), new Item.Properties().rarity(Rarity.COMMON)));
-	public static final RegistryObject<BlockItem> PELECANIMIMUS = REGISTER.register("pelecanimimus_fossil", () -> new BlockItem(FossilBlocks.PELECANIMIMUS.get(), new Item.Properties().rarity(Rarity.COMMON)));
+	public static final RegistryObject<BlockItem> EUNOTOSAURUS = REGISTER.register("eunotosaurus_fossil", () -> new BlockItem(FossilBlocks.EUNOTOSAURUS.get(), new Item.Properties().rarity(FossilRarities.FIVE_PIECE_FOSSIL)));
+	public static final RegistryObject<BlockItem> PELECANIMIMUS = REGISTER.register("pelecanimimus_fossil", () -> new BlockItem(FossilBlocks.PELECANIMIMUS.get(), new Item.Properties().rarity(FossilRarities.FIVE_PIECE_FOSSIL)));
 
 	//Ten
-	public static final RegistryObject<BlockItem> KENTROSAURUS = REGISTER.register("kentrosaurus_fossil", () -> new BlockItem(FossilBlocks.KENTROSAURUS.get(), new Item.Properties().rarity(Rarity.UNCOMMON)));
+	public static final RegistryObject<BlockItem> KENTROSAURUS = REGISTER.register("kentrosaurus_fossil", () -> new BlockItem(FossilBlocks.KENTROSAURUS.get(), new Item.Properties().rarity(FossilRarities.TEN_PIECE_FOSSIL)));
 
 	//Fifteen
-	public static final RegistryObject<BlockItem> WUERHOSAURUS = REGISTER.register("wuerhosaurus_fossil", () -> new BlockItem(FossilBlocks.WUERHOSAURUS.get(), new Item.Properties().rarity(Rarity.RARE)));
+	public static final RegistryObject<BlockItem> WUERHOSAURUS = REGISTER.register("wuerhosaurus_fossil", () -> new BlockItem(FossilBlocks.WUERHOSAURUS.get(), new Item.Properties().rarity(FossilRarities.FIFTEEN_PIECE_FOSSIL)));
 
 	//Twenty
-	public static final RegistryObject<BlockItem> UTAHRAPTOR = REGISTER.register("utahraptor_fossil", () -> new BlockItem(FossilBlocks.UTAHRAPTOR.get(), new Item.Properties().rarity(Rarity.EPIC)));
+	public static final RegistryObject<BlockItem> UTAHRAPTOR = REGISTER.register("utahraptor_fossil", () -> new BlockItem(FossilBlocks.UTAHRAPTOR.get(), new Item.Properties().rarity(FossilRarities.TWENTY_PIECE_FOSSIL)));
 		
 	public static final RegistryObject<BlockItem> PRECAMBRIAN_FOSSIL_ORE = REGISTER.register("precambrian_fossil_ore", () -> new BlockItem(FossilBlocks.PRECAMBRIAN_FOSSIL_ORE.get(), new Item.Properties()));
 	public static final RegistryObject<BlockItem> CAMBRIAN_FOSSIL_ORE = REGISTER.register("cambrian_fossil_ore", () -> new BlockItem(FossilBlocks.CAMBRIAN_FOSSIL_ORE.get(), new Item.Properties()));
---
+++ src/main/java/fossils/fossils/init/FossilRarities.java
@@ -0,0 +1,13 @@
+package fossils.fossils.init;
+
+import net.minecraft.ChatFormatting;
+import net.minecraft.world.item.Rarity;
+
+public class FossilRarities {
+
+	public static final Rarity FIVE_PIECE_FOSSIL = Rarity.create("five_piece_fossil", ChatFormatting.GREEN);
+	public static final Rarity TEN_PIECE_FOSSIL = Rarity.create("ten_piece_fossil", ChatFormatting.BLUE);
+	public static final Rarity FIFTEEN_PIECE_FOSSIL = Rarity.create("fifteen_piece_fossil", ChatFormatting.DARK_PURPLE);
+	public static final Rarity TWENTY_PIECE_FOSSIL = Rarity.create("twenty_piece_fossil", ChatFormatting.GOLD);
+	
+}
